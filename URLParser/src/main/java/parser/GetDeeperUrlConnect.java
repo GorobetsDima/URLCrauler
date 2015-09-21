8772d8da339cff21da1db5e4038b385e7bd3d362
GorobetsDima <007Dragon1988@gmail.com>
2015-09-21 18:26:35 +0300

    newURLParserApp002 commit, another way for GetDeeperUrlConnect

--- URLParser/src/main/java/parser/GetDeeperUrlConnect.java
+++ URLParser/src/main/java/parser/GetDeeperUrlConnect.java
@@ -19,6 +19,10 @@ class GetDeeperUrlConnect implements Runnable {
     //метод который заходит в множество ссылок и записывает в файл их содержимое
     public Set<String> writeToDiffFiles(Set<String> links) {
         HTMLLinkExtractor extractor = new HTMLLinkExtractor();
+        if (!links.iterator().hasNext()) {
+            return null;
+        }
+
 
         Set<String> linksDeep = null;
 
@@ -26,25 +30,48 @@ class GetDeeperUrlConnect implements Runnable {
             String newFileName = "C:\\Users\\Вика\\IdeaProjects\\URLParser\\URLParser\\html.files\\" + links.iterator().next() + ".html";
             String htmlToStringDeep = GetURLContent.writeToFile(links.iterator().next(), newFileName);
             linksDeep = extractor.parseStringBuilder(htmlToStringDeep);
+            links.remove(links.iterator().next());
+            writeToDiffFiles(links);
 
         }
         return linksDeep;
     }
 
+    public Set<String> writeToDiffFiles(String urlAddress, String fileName) {
+        HTMLLinkExtractor extractor = new HTMLLinkExtractor();
+//        if(!links.iterator().hasNext()){
+//            return null;
+//        }
+
+
+        Set<String> links = null;
+        String htmlToStringDeep = GetURLContent.writeToFile(urlAddress, fileName);
+        links = extractor.parseStringBuilder(htmlToStringDeep);
+//        if (links.iterator().hasNext()) {
+        urlAddress = links.iterator().next();
+        fileName = "C:\\Users\\Вика\\IdeaProjects\\URLParser\\URLParser\\html.files\\" + links.iterator().next() + ".html";
+
+
+        links.remove(links.iterator().next());
+        writeToDiffFiles(links);
+
+//        }
+        return links;
+    }
 
 
     @Override
     public void run() {
 
 
-        System.out.println("Thread started:::"+Thread.currentThread().getName());
+        System.out.println("Thread started:::" + Thread.currentThread().getName());
         try {
             writeToDiffFiles(firstLinks);
             Thread.sleep(4000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("Thread ended:::"+Thread.currentThread().getName());
+        System.out.println("Thread ended:::" + Thread.currentThread().getName());
     }
 
 }
