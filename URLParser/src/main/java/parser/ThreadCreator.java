d2cb140ca9f71d76098ebcaff83a97f76892a5d2
Dmitriy Gorobets <007Dragon1988@gmail.com>
2015-10-14 12:02:29 +0300

    Update ThreadCreator.java

--- URLParser/src/main/java/parser/ThreadCreator.java
+++ URLParser/src/main/java/parser/ThreadCreator.java
@@ -10,31 +10,20 @@ public class ThreadCreator {
 
     public static void startThreads (){
 
-//            Thread t = new Thread(new GetDeeperUrlConnect(), "mainThread");
-//            t.start();
-//
-//            try {
-//                t.wait(2000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//            System.out.println("All threads are dead, exiting main thread");
+
+        public static void startThreads (String threadName){
 
 
+            Thread t = new Thread(new GetDeeperUrlConnect(), String.format("%s :" + threadName, "Thread"));
+            t.start();
 
-//        public static void startThreads (String threadName){
-//
-//
-//            Thread t = new Thread(new GetDeeperUrlConnect(), String.format("%s :" + threadName, "Thread"));
-//            t.start();
-//
-//            try {
-//                t.join(5000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//            System.out.println("All threads are dead, exiting main thread");
-//        }
+            try {
+                t.join(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("All threads are dead, exiting main thread");
+       }
 
     }
 }
