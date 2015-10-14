978b0a78cea0641af08b7becea1ab74c8b85f31e
Dmitriy Gorobets <007Dragon1988@gmail.com>
2015-10-14 11:38:46 +0300

    Update CraulerRunner.java

--- URLParser/src/main/java/parser/CraulerRunner.java
+++ URLParser/src/main/java/parser/CraulerRunner.java
@@ -1,14 +1,14 @@
 package parser;
 
 /**
- * Created by ���� on 21.09.2015.
+ * Created by Gorobets Dmitriy on 21.09.2015.
  */
 public class CraulerRunner {
     public static void main(String[] args) {
-//        ThreadCreator.startThreads();
+       ThreadCreator.startThreads();
 
-//        HTMLLinkExtractor html  = new HTMLLinkExtractor();
-//        html.parseStringBuilder(GetURLContent.writeToFile("http://www.mkyong.com", "C:\\Users\\Вика\\IdeaProjects\\URLParser\\URLParser"));
+        HTMLLinkExtractor html  = new HTMLLinkExtractor();
+        html.parseStringBuilder(GetURLContent.writeToFile("http://www.mkyong.com", "C:\\Users\\Вика\\IdeaProjects\\URLParser\\URLParser"));
 
         GetDeeperUrlConnect.writeToDiffFiles(2,"http://www.mkyong.com", "C:\\Users\\Вика\\IdeaProjects\\URLParser\\URLParser");
     }
